fb72714462bb2194b19aaa2043f2135e6386fa8d
blubird <lebaohv1236bqt@gmail.com>
2023-11-24 11:46:24 +0700

    update fix email trans

--- src/services/EmailService.js
+++ src/services/EmailService.js
@@ -6,9 +6,12 @@ var inlineBase64 = require('nodemailer-plugin-inline-base64');
 const sendEmailCreateOrder = async (email,orderItems,orderData,paymentMethod, delivery, isPaid, Payment) => {
 
   const phuongThuc = orderData.shippingMethod === 'nhan tai cua hang' ? "Nhận tại cửa hàng" : "Giao hàng tận nơi"
-  const noteU = orderData.noteUser === null ? orderData.noteUser : "Không có"
+  const noteU = orderData.noteUser === null ? orderData.noteUser : ""
+
   const diachi = orderData.shippingMethod === 'nhan tai cua hang' ? orderData.shopAddress.addressShop : orderData.shippingAddress.addressShipping
   const city = orderData.shippingMethod === 'nhan tai cua hang' ? orderData.shopAddress.cityShop : orderData.shippingAddress.cityShipping
+  const noteship = orderData.shippingMethod === 'nhan tai cua hang' ? orderData.shopAddress.noteShipping : orderData.shippingAddress.noteShipping
+  const ghichu = noteship === null ? noteship : "Không có"
   const paid = isPaid === true ? "Đã thanh toán" : "Chưa thanh toán"
   const donvivanchuyen = orderData.shippingMethod === 'nhan tai cua hang' ? null : delivery
 
@@ -44,6 +47,7 @@ const sendEmailCreateOrder = async (email,orderItems,orderData,paymentMethod, de
     <div><b>Thông tin người đặt hàng:</b> ${orderData.name}, ${orderData.addressUser}, ${orderData.phone}, ${noteU}</div>
     <div><b>Phương thức nhận hàng: ${phuongThuc}</b></div>
     <div><b>Địa chỉ nhận hàng: ${diachi} , ${city}</b></div>
+    <div><b>Ghi chú nhận hàng: ${ghichu}</b></div>
     <div><b>Mã giảm giá (nếu có): ${Payment.coupon.couponPrice}%</b></div>
     <div><b>Phí vận chuyển (nếu có): ${Payment.shippingPrice.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}</b></div>
     <div><b>Đơn vị vận chuyển (nếu có): ${donvivanchuyen}</b></div>
