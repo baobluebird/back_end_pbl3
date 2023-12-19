ad0edd135be7414aec8c7bd108ea60df8f4496f8
baobluebird <lebaohv1236bqt@gmail.com>
2023-12-20 06:57:03 +0700

    model user

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -6,9 +6,9 @@ const userSchema = new mongoose.Schema(
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
         isAdmin: { type: Boolean, default: false, required: true },
+        isAuth: { type: Boolean, default: false, required: true },
         phone: { type: Number },
         avatar: { type: String },
-        isAuth: { type: Boolean, default: false, required: true },
     },
     {
         timestamps: true
